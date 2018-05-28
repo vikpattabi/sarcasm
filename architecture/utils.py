5a94bc21d277362d9c74cf7607cf68d2f40cb89c
Vik Pattabi <vikpattabi@Viks-MacBook-Pro.local>
2018-05-28 00:28:42 -0700

    Set up various stuff

--- architecture/utils.py
+++ architecture/utils.py
@@ -1,2 +1,17 @@
-def words_to_indexes(sentence):
+
+# Define a class containing our language - we want to use the top N common words...
+class language:
+    def __init__(self):
+        self.word_indices = {}
+        self.N = -1
+
+    def process_sentence(self, sentence):
+        pass
+
+    def process_word(self, word):
+        pass
+
+# Words to indices...see example?
+def words_to_indexes(language, sentence):
     sentence = sentence.split(' ')
+    pass
