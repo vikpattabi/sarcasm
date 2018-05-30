b3ad869a58f7db7e8644739222105c6d51080dec
m-hahn <mhahn29@gmail.com>
2018-05-29 18:40:51 -0700

    refactored

--- architecture/training.py
+++ architecture/training.py
@@ -1,66 +1,204 @@
 import random
 import torch
-from utils import START_TOKEN, END_TOKEN
+#from utils import START_TOKEN, END_TOKEN
 
-def train_example(input, output, encoder, decoder, encoder_optimizer, decoder_optimizer, params, loss_fn):
-    encoder_hidden = encoder.initHidden()
-    encoder_optimizer.zero_grad()
-    decoder_optimizer.zero_grad()
+from data import read
 
-    input_length = input.size(0)
-    target_length = output.size(0)
-    loss = 0.0
+def collectAndPadInput(current, index):
+      maxLength = max([len(x[index]) for x in current])
+      context_sentence = []
+      for i in range(maxLength):
+         context_sentence.append([x[index][i] if i < len(x[index]) else 0 for x in current])
+      return context_sentence
 
-    encoder_outs = torch.zeros(params.max_length, encoder.hidden_size)
-    for word in range(input_length):
-        out, encoder_hidden = encoder(input[word], encoder_hidden)
-        encoder_outs[word] = out[0, 0]
 
-    # Whether or not the decoder uses the previous ground truth or decoded output in the next step.
-    teacher_forcing = True if random.random() < params.teacher_forcing else False
+comment_index = read.keys.index("comment")
+parent_index = read.keys.index("parent_comment")
 
-    decoder_hidden = encoder_hidden
-    decoder_in = torch.tensor([[START_TOKEN]])
-    # Assuming we're using the attention decoder here...
-    for word in range(target_length):
-        decoder_out, decoder_hidden, _ = decoder(decoder_in, decoder_hidden, encoder_outs)
-        loss += loss_fn(decoder_out, output[word])
-        if teacher_forcing:
-            decoder_in = output[word]
-        else:
-            vals, indices = topk(decoder_out, 1)
-            decoder_in = indices.squeeze().detach()
 
-        if decoder_in.item() == END_TOKEN:
-            break
 
-    loss.backward()
-    encoder_optimizer.step()
-    decoder_optimizer.step()
-    return loss.item() / target_length
 
-def run_training_loop(params, encoder, decoder):
-    learning_rate = params.learning_rate
-    n_iters = params.n_iters
 
-    encoder_optimizer = None
-    decoder_optimizer = None
-    if config.optimizer == 'Adam':
-        encoder_optimizer = optim.Adam(encoder.parameters(), lr = learning_rate)
-        decoder_optimizer = optim.Adam(decoder.parameters(), lr = learning_rate)
-    else:
-        encoder_optimizer = optim.SGD(encoder.parameters(), lr = learning_rate)
-        decoder_optimizer = optim.SGD(decoder.parameters(), lr = learning_rate)
+def run_training_loop(training_data, held_out_data, encoder, decoder, embeddings, batchSize=32, learning_rate=0.001, optimizer="Adam", useAttention=False, stoi=None, itos=None):
 
-    loss_fn = nn.NLLLoss() # Why this one, TODO: We should think more about this...
-    training_pairs = #TODO: Fill this in...
 
-    for i in range(n_iters):
-        pair = training_pairs[i]
-        input = pair[0]
-        target = pair[1]
+ def predictFromInput(input_sentence):
+    input = read.encode_sentence(input_sentence, stoi)
+    
+    encoder_outputs, hidden = encoder.forward(torch.LongTensor(input).cuda(), None)
+    generated = [torch.LongTensor([0]).cuda()]
+    generated_words = []
+    while True:
+       input = generated[-1]
+       if not useAttention:
+          output, hidden = decoder.forward(input, hidden)
+       else:
+          output, hidden, attention = decoder.forward(input.view(1,1), hidden, encoder_outputs=encoder_outputs)
+          print(attention[0].view(-1).data.cpu().numpy()[:])
+       _, predicted = torch.topk(output, 2, dim=2)
+       predicted = predicted.data.cpu().view(2).numpy()
+       if predicted[0] == 2:
+           predicted = predicted[1]
+       else:
+          predicted = predicted[0]
+       
+       predicted_numeric = predicted
+       if predicted_numeric == 1 or predicted_numeric == 0 or len(generated_words) > 100:
+          return " ".join(generated_words)
+       elif predicted_numeric ==2:
+         generated_words.append("OOV")
+       else:
+         generated_words.append(itos[predicted_numeric-3])
+       generated.append(torch.LongTensor([predicted_numeric]).cuda())
+ 
+ 
+ 
 
-        loss = train_example(input, target, encoder, decoder, encoder_optimizer, decoder_optimizer, params, loss_fn)
+ encoder_optimizer = None
+ decoder_optimizer = None
+ embeddings_optimizer = None
+ 
+ optimizer = "Adam"
+ 
+ 
+ if optimizer == 'Adam':
+     encoder_optimizer = torch.optim.Adam(encoder.parameters(), lr = learning_rate)
+     decoder_optimizer = torch.optim.Adam(decoder.parameters(), lr = learning_rate)
+     embeddings_optimizer = torch.optim.Adam(embeddings.parameters(), lr = learning_rate)
+ else:
+     encoder_optimizer = torch.optim.SGD(encoder.parameters(), lr = learning_rate)
+     decoder_optimizer = torch.optim.SGD(decoder.parameters(), lr = learning_rate)
+     embeddings_optimizer = torch.optim.SGD(embeddings.parameters(), lr = learning_rate)
+ 
+ lossModule = torch.nn.NLLLoss(ignore_index=0)
+ lossModuleNoAverage = torch.nn.NLLLoss(size_average=False, ignore_index=0)
 
-        if i % params.print_every == 0:
-            print('Iteration %d of %d, loss is: %.4f' % (i, n_iters, loss))
+
+
+ devLosses = []
+
+ batchSize = 32
+
+ training_partitions = list(range(int(len(training_data)/batchSize)))
+
+
+
+ for epoch in range(1000):
+
+   random.shuffle(training_partitions)
+
+   encoder.train(True) # set to training mode (make sure dropout is turned on again after running on dev set)
+   decoder.train(True)
+  
+   steps = 0
+   crossEntropy = 10
+   for partition in training_partitions:
+      current = training_data[partition*batchSize:(partition+1)*batchSize] # reads a minibatch of length batchSize
+      steps += 1
+      
+      encoder_optimizer.zero_grad()
+      decoder_optimizer.zero_grad()
+      embeddings_optimizer.zero_grad()
+   
+
+      context_sentence = collectAndPadInput(current, parent_index)
+      response_sentence = collectAndPadInput(current, comment_index)
+
+
+      encoder_outputs, hidden = encoder.forward(torch.LongTensor(context_sentence).cuda(), None)
+
+      response = torch.LongTensor(response_sentence).cuda()
+     
+
+      if not useAttention:
+         output, _ = decoder.forward(response[:-1], hidden)
+      else:
+         output, _, attentions = decoder.forward(response[:-1], hidden, encoder_outputs=encoder_outputs)
+
+      loss = lossModule(output.view(-1, 10000+3), response[1:].view(-1))
+
+
+      crossEntropy = 0.99 * crossEntropy + (1-0.99) * loss.data.cpu().numpy()
+   
+      loss.backward()
+      encoder_optimizer.step()
+      decoder_optimizer.step()
+      embeddings_optimizer.step()
+   
+   
+      if steps % 1000 == 0:
+          print((epoch,steps,crossEntropy))
+          print(devLosses)
+          print(predictFromInput(["This", "article", "is", "such", "BS", "."]))
+          print(predictFromInput(["This", "article", "is", "awesome", "."]))
+          print(predictFromInput(["Bankers", "celebrate", "the", "start", "of", "the", "Trump", "era", "."]))
+
+   # At the end of every epoch, we run on the development partition and record the log-likelihood. As soon as it drops, we stop training
+   print("Running on dev")
+   encoder.train(False)
+   decoder.train(False)
+   steps = 0
+   totalLoss = 0
+   numberOfWords = 0
+   for dataPoint in held_out_data:
+      steps += 1
+   
+      numberOfWords += len(dataPoint[comment_index])-1
+
+      encoder_outputs, hidden = encoder.forward(torch.LongTensor(dataPoint[parent_index]).cuda(), None)
+      target = torch.LongTensor(dataPoint[comment_index][1:]).view(-1).cuda()
+
+      if not useAttention:
+         output, _ = decoder.forward(torch.LongTensor(dataPoint[comment_index][:-1]).cuda(), hidden)
+      else:
+         output, _, attentions = decoder.forward(torch.LongTensor(dataPoint[comment_index][:-1]).view(-1,1).cuda(), hidden, encoder_outputs=encoder_outputs)
+      loss = lossModuleNoAverage(output.view(-1, 10000+3), target.view(-1))
+
+      crossEntropy = 0.99 * crossEntropy + (1-0.99) * loss.data.cpu().numpy()
+      totalLoss +=  loss.data.cpu().numpy()
+   devLosses.append(totalLoss/numberOfWords)
+   print(devLosses)
+   if len(devLosses) > 1 and devLosses[-1] > devLosses[-2]:
+       print("Overfitting, stop")
+       return
+
+
+
+#def train_example(input, output, encoder, decoder, encoder_optimizer, decoder_optimizer, params, loss_fn):
+#    encoder_hidden = encoder.initHidden()
+#    encoder_optimizer.zero_grad()
+#    decoder_optimizer.zero_grad()
+#
+#    input_length = input.size(0)
+#    target_length = output.size(0)
+#    loss = 0.0
+#
+#    encoder_outs = torch.zeros(params.max_length, encoder.hidden_size)
+#    for word in range(input_length):
+#        out, encoder_hidden = encoder(input[word], encoder_hidden)
+#        encoder_outs[word] = out[0, 0]
+#
+#    # Whether or not the decoder uses the previous ground truth or decoded output in the next step.
+#    teacher_forcing = True if random.random() < params.teacher_forcing else False
+#
+#    decoder_hidden = encoder_hidden
+#    decoder_in = torch.tensor([[START_TOKEN]])
+#    # Assuming we're using the attention decoder here...
+#    for word in range(target_length):
+#        decoder_out, decoder_hidden, _ = decoder(decoder_in, decoder_hidden, encoder_outs)
+#        loss += loss_fn(decoder_out, output[word])
+#        if teacher_forcing:
+#            decoder_in = output[word]
+#        else:
+#            vals, indices = topk(decoder_out, 1)
+#            decoder_in = indices.squeeze().detach()
+#
+#        if decoder_in.item() == END_TOKEN:
+#            break
+#
+#    loss.backward()
+#    encoder_optimizer.step()
+#    decoder_optimizer.step()
+#    return loss.item() / target_length
+#
+
--- main.py
+++ main.py
@@ -5,31 +5,26 @@ from data import read
 
 from architecture import model
 
-
+# Read the word list
 # itos is a list of words
 # stoi (dict) maps words (strings) to their position in itos (int)
-itos, stoi = read.createVocabulary(vocab_size=10000)
+itos, stoi = read.createVocabulary(vocab_size=10000) # restrict to 10000 most frequent words
 print("Read dictionary")
 
+# Reading the data that we have extracted to far
 training_data, held_out_data = read.readTrainingAndDevData(stoi)
-
-
-
 print("Read training data.")
-
-
-import random
-
 print("Length of training set "+str(len(training_data)))
 
 
+from architecture import training
+import random
 import torch
 
 
 useGlove = True
 
 embeddings = model.embeddings(vocab_size=10000+3, embedding_size=100).cuda()
-
 if useGlove:
    embeddings.weight.data.copy_(torch.FloatTensor(read.loadGloveEmbeddings(stoi)).cuda())
 
@@ -43,162 +38,10 @@ if useAttention:
   decoder = model.attentionDecoderRNN(hidden_size=200, embedding_size=100, embeddings=embeddings, vocab_size=10000+3).cuda()
 else:
   decoder = model.decoderRNN(hidden_size=200, embedding_size=100, embeddings=embeddings, vocab_size=10000+3).cuda()
-lossModule = torch.nn.NLLLoss(ignore_index=0)
-lossModuleNoAverage = torch.nn.NLLLoss(size_average=False, ignore_index=0)
-
-# Visualize attention matrices
-
-learning_rate = 0.001
 
 
 
-encoder_optimizer = None
-decoder_optimizer = None
-embeddings_optimizer = None
 
-optimizer = "Adam"
-
-
-if optimizer == 'Adam':
-    encoder_optimizer = torch.optim.Adam(encoder.parameters(), lr = learning_rate)
-    decoder_optimizer = torch.optim.Adam(decoder.parameters(), lr = learning_rate)
-    embeddings_optimizer = torch.optim.Adam(embeddings.parameters(), lr = learning_rate)
-else:
-    encoder_optimizer = torch.optim.SGD(encoder.parameters(), lr = learning_rate)
-    decoder_optimizer = torch.optim.SGD(decoder.parameters(), lr = learning_rate)
-    embeddings_optimizer = torch.optim.SGD(embeddings.parameters(), lr = learning_rate)
-
-
-def predictFromInput(input_sentence):
-   input = read.encode_sentence(input_sentence, stoi)
-   
-   encoder_outputs, hidden = encoder.forward(torch.LongTensor(input).cuda(), None)
-   generated = [torch.LongTensor([0]).cuda()]
-   generated_words = []
-   while True:
-      input = generated[-1]
-      if not useAttention:
-         output, hidden = decoder.forward(input, hidden)
-      else:
-         output, hidden, attention = decoder.forward(input.view(1,1), hidden, encoder_outputs=encoder_outputs)
-         print(attention[0].view(-1).data.cpu().numpy()[:])
-      _, predicted = torch.topk(output, 2, dim=2)
-      predicted = predicted.data.cpu().view(2).numpy()
-      if predicted[0] == 2:
-          predicted = predicted[1]
-      else:
-         predicted = predicted[0]
-      
-      predicted_numeric = predicted
-      if predicted_numeric == 1 or predicted_numeric == 0 or len(generated_words) > 100:
-         return " ".join(generated_words)
-      elif predicted_numeric ==2:
-        generated_words.append("OOV")
-      else:
-        generated_words.append(itos[predicted_numeric-3])
-      generated.append(torch.LongTensor([predicted_numeric]).cuda())
-
-
-comment_index = read.keys.index("comment")
-parent_index = read.keys.index("parent_comment")
-
-
-training_data = sorted(training_data, key = lambda x:(len(x[comment_index]), len(x[parent_index])))
-held_out_data = sorted(held_out_data, key = lambda x:(len(x[comment_index]), len(x[parent_index])))
-
-
-
-devLosses = []
-
-batchSize = 32
-
-training_partitions = list(range(int(len(training_data)/batchSize)))
-
-def collectAndPadInput(current, index):
-      maxLength = max([len(x[index]) for x in current])
-      context_sentence = []
-      for i in range(maxLength):
-         context_sentence.append([x[index][i] if i < len(x[index]) else 0 for x in current])
-      return context_sentence
-
-
-
-for epoch in range(1000):
-
-   random.shuffle(training_partitions)
-
-   encoder.train(True) # set to training mode (make sure dropout is turned on again after running on dev set)
-   decoder.train(True)
-  
-   steps = 0
-   crossEntropy = 10
-   for partition in training_partitions:
-      current = training_data[partition*batchSize:(partition+1)*batchSize] # reads a minibatch of length batchSize
-      steps += 1
-      
-      encoder_optimizer.zero_grad()
-      decoder_optimizer.zero_grad()
-      embeddings_optimizer.zero_grad()
-   
-
-      context_sentence = collectAndPadInput(current, parent_index)
-      response_sentence = collectAndPadInput(current, comment_index)
-
-
-      encoder_outputs, hidden = encoder.forward(torch.LongTensor(context_sentence).cuda(), None)
-
-      response = torch.LongTensor(response_sentence).cuda()
-     
-
-      if not useAttention:
-         output, _ = decoder.forward(response[:-1], hidden)
-      else:
-         output, _, attentions = decoder.forward(response[:-1], hidden, encoder_outputs=encoder_outputs)
-
-      loss = lossModule(output.view(-1, 10000+3), response[1:].view(-1))
-
-
-      crossEntropy = 0.99 * crossEntropy + (1-0.99) * loss.data.cpu().numpy()
-   
-      loss.backward()
-      encoder_optimizer.step()
-      decoder_optimizer.step()
-      embeddings_optimizer.step()
-   
-   
-      if steps % 1000 == 0:
-          print((epoch,steps,crossEntropy))
-          print(devLosses)
-          print(predictFromInput(["This", "article", "is", "such", "BS", "."]))
-          print(predictFromInput(["This", "article", "is", "awesome", "."]))
-          print(predictFromInput(["Bankers", "celebrate", "the", "start", "of", "the", "Trump", "era", "."]))
-
-   print("Running on dev")
-   encoder.train(False)
-   decoder.train(False)
-   steps = 0
-   totalLoss = 0
-   numberOfWords = 0
-   for dataPoint in held_out_data:
-      steps += 1
-   
-      numberOfWords += len(dataPoint[comment_index])-1
-
-      encoder_outputs, hidden = encoder.forward(torch.LongTensor(dataPoint[parent_index]).cuda(), None)
-      target = torch.LongTensor(dataPoint[comment_index][1:]).view(-1).cuda()
-
-      if not useAttention:
-         output, _ = decoder.forward(torch.LongTensor(dataPoint[comment_index][:-1]).cuda(), hidden)
-      else:
-         output, _, attentions = decoder.forward(torch.LongTensor(dataPoint[comment_index][:-1]).view(-1,1).cuda(), hidden, encoder_outputs=encoder_outputs)
-      loss = lossModuleNoAverage(output.view(-1, 10000+3), target.view(-1))
-
-      crossEntropy = 0.99 * crossEntropy + (1-0.99) * loss.data.cpu().numpy()
-      totalLoss +=  loss.data.cpu().numpy()
-   devLosses.append(totalLoss/numberOfWords)
-   print(devLosses)
-   if len(devLosses) > 1 and devLosses[-1] > devLosses[-2]:
-       print("Overfitting, stop")
-       quit()
+training.run_training_loop(training_data, held_out_data, encoder, decoder, embeddings, batchSize=32, learning_rate=0.001, optimizer="Adam", useAttention=useAttention, stoi=stoi, itos=itos)
 
 
